a911585e7c6b2ad521da15017a07eb0db412dbcc
Jimmy <jimmy9065@hotmail.com>
2018-02-22 04:30:37 -0800

    updated

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
 var fs = require('fs');
 var HashMap = require('hashmap');
 const scraper = require('./scraper.js');
-var queue = [];
-var map = new HashMap();
 
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 let crawling = async function(browser){
-  let key = '销售易';
-  //let key = '微盟';
-  let res = await scraper.query(browser, key);
-  map.set(key, res);
-  queue = res;
+  let key;
+  let queue = [];
+  let map = new HashMap();
+  if(fs.existsSync('./candidate') && fs.existsSync('./graph')){
+    queue = fs.readFileSync('./candidate', "utf-8").split(/\n/);
+    console.log('found cadidate');
+    console.log(queue);
+  }
+  else{
+    key = '销售易';
+    queue = await scraper.query(browser, key);
+    map.set(key, queue);
+  }
+
+  if(fs.existsSync('./candidate')){
+    console.log('delete cadidate file')
+    fs.unlinkSync('./candidate');
+  }
+
+  let fsGraph = fs.createWriteStream('./graph')
   
   while(queue.length > 0){
     await timeout(1000);
     //key = queue.pop();
     key = queue.shift();
     if(!map.has(key)){
-      let res = await scraper.query(browser, key);
-      map.set(key, res);
-      for (let idx in res){
-        if(!map.has(res[idx])){
-          queue.push(res[idx]);
+      try {
+        let res = await scraper.query(browser, key);
+        let data = key + ":";
+        map.set(key, res);
+        for (let idx in res){
+          if(!map.has(res[idx])){
+            queue.push(res[idx]);
+          }
+          data += res[idx] + ",";
         }
+        fsGraph.write(data, 'UTF8');
+      } 
+      catch( error) {
+        console.log('spider failed');
+        break;
       }
     }
   }
 
-  scraper.stopBrowser(browser);
+  fsGraph.end();
+  let w_data = new Buffer(queue.join('/n'));
+  fs.writeFile('./candidate', w_data, {flag: 'a'}, function(err) {
+    if(err)
+      console.log(err);
+    else{
+      console.log('candidate file recorded');
+      scraper.stopBrowser(browser);
+    }
+  })
 };
 
 scraper.startBrowser()
